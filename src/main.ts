db9bc522332ce398a3e372b73527520b1e12e425
Charles Wang <zwcharl@gmail.com>
2024-12-04 18:06:21 -0500

    infinitely scrolling platforms

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import GUI from "lil-gui";
+import Stats from "stats.js";
 import * as THREE from "three";
 
 import "@/src/style.css";
+import { Timer } from "three/addons/misc/Timer.js";
 
 // Globals
 const canvas = document.querySelector<HTMLCanvasElement>("#c")!;
 const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
 const camera = new THREE.PerspectiveCamera(50, 2, 0.1, 100);
 const scene = new THREE.Scene();
+
+const platforms: THREE.Mesh[] = [];
+const platformWidth = 2;
+let offset = 0;
+let elapsed = 0;
 
 function setupGUI() {
   const controls = {
@@ -22,21 +29,46 @@ function setupGUI() {
   return { color, speed };
 }
 
+function setupStats() {
+  const stats = new Stats();
+  stats.showPanel(0);
+  document.body.appendChild(stats.dom);
+
+  return stats;
+}
+
 function main() {
-  const { color, speed } = setupGUI();
+  const stats = setupStats();
+  const timer = new Timer();
 
-  camera.position.z = 5;
+  const geometry = new THREE.BoxGeometry(platformWidth, 1, 1);
 
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshLambertMaterial({ color: color.getValue() });
-  const cube = new THREE.Mesh(geometry, material);
+  function initScene() {
+    camera.position.z = 8;
+    camera.position.y = 2;
 
-  scene.add(cube);
+    const light = new THREE.DirectionalLight(0xffffff, 3);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
 
-  const light = new THREE.DirectionalLight(0xffffff, 3);
-  light.position.set(-1, 2, 4);
+    // Positioned at origin, offset remains 0
+    const mat1 = new THREE.MeshLambertMaterial({ color: 0xff0000 });
+    const cube1 = new THREE.Mesh(geometry, mat1);
+    scene.add(cube1);
 
-  scene.add(light);
+    // Offset by geometry width
+    offset += platformWidth;
+
+    // Second platform, so begin using offset to translate
+    const mat2 = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
+    const cube2 = new THREE.Mesh(geometry, mat2);
+    cube2.translateX(offset);
+    scene.add(cube2);
+
+    offset += platformWidth;
+
+    platforms.push(cube1, cube2);
+  }
 
   function resizeRendererToDisplaySize() {
     const width = canvas.clientWidth;
@@ -51,19 +83,38 @@ function main() {
   }
 
   function render(time: number) {
-    time *= 0.001;
-    console.log("time:", time);
+    timer.update(time);
+    elapsed += timer.getDelta();
 
-    cube.rotation.x += speed.getValue();
-    cube.rotation.y += speed.getValue();
-    cube.material.setValues({ color: color.getValue() });
+    if (elapsed > 3) {
+      const newMat = new THREE.MeshLambertMaterial({
+        color: new THREE.Color(Math.random(), Math.random(), Math.random()),
+      });
+      const newCube = new THREE.Mesh(geometry, newMat);
+      newCube.translateX(offset);
+
+      platforms.push(newCube);
+      scene.add(newCube);
+      offset += platformWidth;
+
+      console.log("added new cube, reset at", timer.getElapsed());
+      elapsed = 0;
+    }
+
+    camera.position.x += 0.005;
 
     resizeRendererToDisplaySize();
 
     renderer.render(scene, camera);
   }
 
-  renderer.setAnimationLoop(render);
+  initScene();
+
+  renderer.setAnimationLoop((time) => {
+    stats.begin();
+    render(time);
+    stats.end();
+  });
 }
 
 main();
