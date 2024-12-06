406148d49a50656eb6569f00d0ad2084c6471be8
Charles Wang <zwcharl@gmail.com>
2024-12-06 08:55:15 -0500

    benches

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import Stats from "stats.js";
 import * as THREE from "three";
 
 import { Timer } from "three/addons/misc/Timer.js";
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { GLTFLoader, type GLTF } from "three/addons/loaders/GLTFLoader.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 import "@fontsource-variable/inter";
@@ -20,6 +20,7 @@ renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.toneMappingExposure = 2;
 
 let platformModel: THREE.Object3D;
+let benchModel: THREE.Object3D;
 let offset = 0;
 
 const platformWidth = 10;
@@ -54,125 +55,93 @@ function setupStats() {
 //   console.log("Scene count:", scene.children.length);
 // }
 
-function main() {
+async function main() {
   const { speed, intensity, y } = setupGUI();
   const stats = setupStats();
   const timer = new Timer();
 
-  function loadPlatform() {
-    const progressText = document.querySelector<HTMLParagraphElement>("#progress")!;
-    const loadingScreen = document.querySelector<HTMLDivElement>("#loading")!;
+  const progressText = document.querySelector<HTMLParagraphElement>("#progress")!;
+  const loadingScreen = document.querySelector<HTMLDivElement>("#loading")!;
 
+  {
     const loader = new GLTFLoader();
+
     loader.manager.onProgress = (_, loaded, total) => {
       progressText.innerText = `${Math.round((loaded / total) * 100)}%`;
     };
 
-    loader.load(
-      "/_models/platform_v2.glb",
+    async function helper(url: string, callback: (data: GLTF) => void) {
+      await loader
+        .loadAsync(url)
+        .then(callback)
+        .catch((err) => console.error(`Error loading "${url}":`, err));
+    }
 
-      ({ scene: wrappers }) => {
-        // Initial scene has a lot of wrappers for some reason
-        platformModel = wrappers.children[0].children[0];
+    await helper("_models/platform_v2.glb", ({ scene: wrappers }) => {
+      // Initial scene has a lot of wrappers for some reason
+      platformModel = wrappers.children[0].children[0];
 
-        // Fix some initial import transforms
-        platformModel.scale.set(1, 1, 1);
-        platformModel.rotation.y = Math.PI;
+      // Fix some initial import transforms
+      platformModel.rotation.y = Math.PI;
 
-        // Traverse the scene to turn on shadows for all objects
-        platformModel.traverse((obj) => {
-          if (obj.castShadow !== undefined) {
-            obj.receiveShadow = true;
-            obj.castShadow = true;
-          }
-        });
+      // Traverse the scene to turn on shadows for all objects
+      platformModel.traverse((obj) => {
+        if (obj.castShadow !== undefined) {
+          obj.receiveShadow = true;
+          obj.castShadow = true;
+        }
+      });
 
-        // Remove onProgress listener from default loading manager
-        loader.manager.onProgress = () => {};
+      console.log("Platform:", platformModel);
+    });
 
-        progressText.innerText = "100%";
-        loadingScreen.classList.add("fade");
-        loadingScreen.addEventListener("transitionend", () => {
-          loadingScreen.remove();
-        });
+    await helper("_models/bench.glb", ({ scene: wrappers }) => {
+      benchModel = wrappers.children[0].children[0];
+      benchModel.position.set(-1.5, -1.75, 0);
+      benchModel.rotation.y = Math.PI / 2;
 
-        console.log("Platform:", platformModel);
-        initScene();
-      },
+      benchModel.traverse((obj) => {
+        if (obj.castShadow !== undefined) {
+          obj.receiveShadow = true;
+          obj.castShadow = true;
+        }
+      });
 
-      // Use default loading manager's onProgress, not this one
-      undefined,
-
-      (error) => {
-        console.error("Error loading platform:", error);
-      },
-    );
+      console.log("Bench:", benchModel);
+    });
   }
+
+  let benchCounter = 0;
 
   function addPlatform(z: number) {
-    const model = platformModel.clone();
-    model.position.z = z;
+    const platformCopy = platformModel.clone();
+    platformCopy.position.z = z;
 
-    scene.add(model);
-    activePlatforms.push(model);
+    if (benchCounter % 3 === 0) {
+      let benchCopy = benchModel.clone();
+      benchCopy.position.z = z;
+      scene.add(benchCopy);
+
+      if (Math.random() >= 0.3) {
+        benchCopy = benchModel.clone();
+        benchCopy.position.z = z + 5.25;
+        scene.add(benchCopy);
+      }
+    }
+
+    benchCounter += 1;
+    scene.add(platformCopy);
+    activePlatforms.push(platformCopy);
   }
 
-  function initScene() {
+  {
     // Initial scene has five platforms visible, offset -20 and +20 around origin
     for (let i = -2; i <= 2; ++i) addPlatform(i * 10);
 
     // Manually update offset this time
     offset = 30;
-    console.log("Active platforms after initScene():", activePlatforms);
+    console.log("Initialized scene with:", activePlatforms);
   }
-
-  function resizeRendererToDisplaySize() {
-    const width = canvas.clientWidth;
-    const height = canvas.clientHeight;
-    const needResize = canvas.width !== width || canvas.height !== height;
-
-    if (needResize) {
-      renderer.setSize(width, height, false);
-      camera.aspect = canvas.clientWidth / canvas.clientHeight;
-      camera.updateProjectionMatrix();
-    }
-  }
-
-  let addElapsed = 0;
-  function checkToAddPlatform() {
-    if (addElapsed >= 1.5 && platformModel) {
-      addPlatform(offset);
-      offset += platformWidth;
-      addElapsed = 0;
-    }
-  }
-
-  let rmElapsed = 0;
-  function checkToRemovePlatform() {
-    if (rmElapsed >= 3) {
-      const model = activePlatforms.shift()!;
-      scene.remove(model);
-      rmElapsed = 0;
-    }
-  }
-
-  function render(time: number) {
-    timer.update(time);
-    addElapsed += timer.getDelta();
-    rmElapsed += timer.getDelta();
-
-    checkToAddPlatform();
-    checkToRemovePlatform();
-
-    camera.position.z += speed.getValue();
-    dirLight.position.z = camera.position.z;
-    dirLight.target.position.z = camera.position.z;
-
-    resizeRendererToDisplaySize();
-    renderer.render(scene, camera);
-  }
-
-  loadPlatform();
 
   const controls = new OrbitControls(camera, renderer.domElement);
   camera.position.y = 7;
@@ -180,6 +149,7 @@ function main() {
   camera.position.x = -18;
   // controls.update();
 
+  // Setup lights
   const dirLight = new THREE.DirectionalLight(0xffffff, 0.15);
   dirLight.position.copy(camera.position);
   dirLight.target.position.set(0, camera.position.y, camera.position.z);
@@ -196,10 +166,54 @@ function main() {
   const ambientLight = new THREE.AmbientLight(0xffffff, 0.75);
   scene.add(ambientLight);
 
+  let addElapsed = 0;
+  let rmElapsed = 0;
+
+  function render(time: number) {
+    timer.update(time);
+    addElapsed += timer.getDelta();
+    rmElapsed += timer.getDelta();
+
+    if (addElapsed >= 1.5 && platformModel) {
+      addPlatform(offset);
+      offset += platformWidth;
+      addElapsed = 0;
+    }
+
+    if (rmElapsed >= 3) {
+      const model = activePlatforms.shift()!;
+      scene.remove(model);
+      rmElapsed = 0;
+    }
+
+    camera.position.z += speed.getValue();
+    dirLight.position.z = camera.position.z;
+    dirLight.target.position.z = camera.position.z;
+
+    // Check if we need to resize renderer to new display size
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+
+    if (needResize) {
+      renderer.setSize(width, height, false);
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+
+    renderer.render(scene, camera);
+  }
+
   renderer.setAnimationLoop((time) => {
     stats.begin();
     render(time);
     stats.end();
+  });
+
+  progressText.innerText = "100%";
+  loadingScreen.classList.add("fade");
+  loadingScreen.addEventListener("transitionend", () => {
+    loadingScreen.remove();
   });
 }
 
